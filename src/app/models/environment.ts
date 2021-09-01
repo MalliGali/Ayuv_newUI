cfee29f267b0646174bc0c422e17ee3746ea5859
Ravi Doki <ravidoki@gmail.com>
2021-09-01 13:44:34 +0100

    Update environment.ts

--- src/app/models/environment.ts
+++ src/app/models/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
     // baseUrl : `https://localhost:8443`
     // baseUrl : `https://backend.ayuv.co.uk:8443`
     // baseUrl : `http://ayuvserver.ddns.net:8010`,
-    baseUrl: `https://dev.ayuv.co.uk:8443`
+    baseUrl: `https://ayuv.co.uk:8443`
 }
